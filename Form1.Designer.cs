ecb2f0b57ccd7ce3e18089c744adaa7e4fcaaa4b
phamhoathuytien <phttien270619@gmail.com>
2026-03-09 00:27:24 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace bai1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.btnTong = new System.Windows.Forms.Button();
+            this.btnLamlai = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.lblKQ = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblA.Location = new System.Drawing.Point(166, 112);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(54, 16);
+            this.lblA.TabIndex = 0;
+            this.lblA.Text = "Nhập a:";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblB.Location = new System.Drawing.Point(166, 200);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(54, 16);
+            this.lblB.TabIndex = 1;
+            this.lblB.Text = "Nhập b:";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(274, 112);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(375, 22);
+            this.txtA.TabIndex = 2;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(274, 200);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(375, 22);
+            this.txtB.TabIndex = 3;
+            // 
+            // btnTong
+            // 
+            this.btnTong.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnTong.Location = new System.Drawing.Point(131, 354);
+            this.btnTong.Name = "btnTong";
+            this.btnTong.Size = new System.Drawing.Size(75, 23);
+            this.btnTong.TabIndex = 4;
+            this.btnTong.Text = "Tổng";
+            this.btnTong.UseVisualStyleBackColor = false;
+            this.btnTong.Click += new System.EventHandler(this.btnTong_Click);
+            // 
+            // btnLamlai
+            // 
+            this.btnLamlai.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLamlai.Location = new System.Drawing.Point(342, 354);
+            this.btnLamlai.Name = "btnLamlai";
+            this.btnLamlai.Size = new System.Drawing.Size(75, 23);
+            this.btnLamlai.TabIndex = 5;
+            this.btnLamlai.Text = "Làm lại";
+            this.btnLamlai.UseVisualStyleBackColor = false;
+            this.btnLamlai.Click += new System.EventHandler(this.btnLamlai_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnThoat.Location = new System.Drawing.Point(574, 354);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 6;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // lblKQ
+            // 
+            this.lblKQ.AutoSize = true;
+            this.lblKQ.Location = new System.Drawing.Point(198, 284);
+            this.lblKQ.Name = "lblKQ";
+            this.lblKQ.Size = new System.Drawing.Size(160, 16);
+            this.lblKQ.TabIndex = 7;
+            this.lblKQ.Text = "Tổng các số từ a đến b là:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(768, 487);
+            this.Controls.Add(this.lblKQ);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnLamlai);
+            this.Controls.Add(this.btnTong);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Button btnTong;
+        private System.Windows.Forms.Button btnLamlai;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label lblKQ;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTong_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(txtA.Text);
+            int b = int.Parse(txtB.Text);
+
+            int start = Math.Min(a, b);
+            int end = Math.Max(a, b);
+
+            int tong = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                tong += i;
+            }
+
+            lblKQ.Text = "Tổng các số từ " + start + " đến " + end + " là " + tong;
+        }
+
+        private void btnLamlai_Click(object sender, EventArgs e)
+        {
+            txtA.Clear();
+            txtB.Clear();
+            lblKQ.Text = "";
+            txtA.Focus();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
